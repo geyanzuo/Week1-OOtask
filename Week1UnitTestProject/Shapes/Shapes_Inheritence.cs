d9f146bdcd2c10de56b0ea46d2c9c06a034cf7ca
(Charlie)YanzuoGe <42125596+geyanzuo@users.noreply.github.com>
2020-02-28 14:11:12 +1100

    update

---
+++ Week1UnitTestProject/Shapes/Shapes_Inheritence.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes_Inheritence
+{
+    public abstract class Shape
+    {
+        public string _name;
+        public string _colour;
+
+        public Shape(string pname, string pcolour)
+        {
+            _name = pname;
+            _colour = pcolour;
+        }
+
+        public abstract double GetArea();
+        public abstract double GetPerimeter();
+
+
+    }
+
+    public abstract class Quadrilateral : Shape
+    {
+        private const int _NumberSides = 4;
+
+        public Quadrilateral(string pname, string pcolour) : base(pname, pcolour)
+        {
+
+        }
+    }
+
+    public class Square : Quadrilateral
+    {
+        public double _sideLength;
+
+        public Square(string pname, string pcolour, double psideLength) : base(pname, pcolour)
+        {
+            _sideLength = psideLength;
+        }
+
+        public override double GetArea()
+        {
+            return Math.Round(_sideLength * _sideLength, 2);
+        }
+
+        public override double GetPerimeter()
+        {
+            return Math.Round( 4 * _sideLength,2);
+        }
+    }
+
+    public class Rectangle : Quadrilateral
+    {
+        public double _length;
+        public double _width;
+
+        public Rectangle(string pname, string pcolour, double plength, double pwidth) : base(pname, pcolour)
+        {
+            _length = plength;
+            _width = pwidth;
+        }
+
+        public override double GetArea()
+        {
+            return Math.Round(_length * _width,2) ;
+        }
+
+        public override double GetPerimeter()
+        {
+            return Math.Round(2 * _length + 2 * _width,2);
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public const double PI = 3.142;
+        public double _radius;
+
+        public Circle(string pname, string pcolour, double pradius) : base(pname, pcolour)
+        {
+            _radius = pradius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.Round( _radius * _radius * PI,2);
+        }
+
+        public override double GetPerimeter()
+        {
+            return Math.Round(2 * _radius * PI,2);
+        }
+
+        public double GetCircumference()
+        {
+            return Math.Round( 2 * _radius * PI,2);
+        }
+    }
+}
---
+++ Week1UnitTestProject/Week1UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Week1UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Week1UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f674484-7366-4a86-9171-77d4ac91cb4f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
